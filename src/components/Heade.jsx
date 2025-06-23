703a185de8016b546c511a023cac4b761f8905ab
paria <pariyataba@gmail.com>
2025-06-23 14:47:27 +0200

    koppla ihåp login sidan med start

--- src/components/Heade.jsx
+++ src/components/Heade.jsx
@@ -2,6 +2,7 @@ import vector from "../images/vector.png"
 import heart from "../images/heart.png"
 import basket from "../images/basket.png"
 import "../components/Header.css"
+import { NavLink } from "react-router-dom"
 function Header(){
 	return(
 		<div className="header">
@@ -9,7 +10,7 @@ function Header(){
 			<span className="color-orange big-font f-font">F</span><span className="color-orange">i</span>academe</div>
 			<div className="border">Discover your abilities and start with us</div>
 			<div className="icon">
-				<img src={vector} />
+				<NavLink to="/login"><img src={vector} /></NavLink>
 				<img src={heart} />
 				<img src={basket} />
 
