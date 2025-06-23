f819740057065b20219626442d4bf3b0f03ae894
paria <pariyataba@gmail.com>
2025-06-23 14:32:26 +0200

    main in hemsidan

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,29 @@
 import "../pages/Home.css"
 import Header from "../components/Heade";
+import "../pages/Home.css"
 function Home(){
 	return(
 		<div className="container">
-			<Header></Header>
+		<Header></Header>
+		<div className="main">
+		<div className="circle">
+		<p className="background-none">Everything</p>
+		<p className="background-none"> You Need 
+		</p>
+		<p className="background-none">to Practice
+		</p>
+		
+		</div>
+		<div className="main-text">
+		<p>More than 10 languages</p>
+		<p>Over 2000 words</p>
+		<p>Thousands of phrases</p>
+		<p>Over 1 Million voice</p>
+		<p>Over 1 Million Sentences</p>
+		<p>Learn languages easily with us</p>
+		</div>
+		</div>
+		
 		</div>
 	)
 }
