65bf2f1c71ab6f50f66fe358034380d528bf2955
FBetulDemir <fatmabetuld@gmail.com>
2025-06-24 11:17:33 +0200

    Add Registration component with form handling and API integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { HashRouter,Routes,Route } from "react-router-dom"
 import Home from "./pages/Home"
 import LoginForm from "./components/LoginForm"
+import Registration from "./components/Registration.jsx"
 
 function App() {
 
   return (
-	<HashRouter>
-		<Routes>
-			<Route path="/" element={<Home></Home>}></Route>
-			<Route path="/login" element={<LoginForm />}></Route>
-		</Routes>
-	</HashRouter>
-
+	<div className="wrapper">
+		<Registration />
+	</div>
+	// <HashRouter>
+	// 	<Routes>
+	// 		<Route path="/" element={<Home></Home>}></Route>
+	// 		<Route path="/login" element={<LoginForm />}></Route>
+	// 	</Routes>
+	// </HashRouter>
+	
 
 
   )
