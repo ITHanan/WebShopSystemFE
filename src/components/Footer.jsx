3e9b768f190ae8bcf6e9f959bf62a6feb4e50015
paria <pariyataba@gmail.com>
2025-06-26 09:22:46 +0200

    design in hemsidan

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,11 +7,11 @@ function Footer(){
 		<img src="/src/images/ring.png" />
 		<p className="title">Quick Links</p>
 		</div>
-		<p>Home</p>
-		<p>Courses</p>
-		<p>Blogs</p>
-		<p>About us</p>
-		<p>Contact</p>
+		<p className="link">Home</p>
+		<p className="link">Courses</p>
+		<p className="link">Blogs</p>
+		<p className="link">About us</p>
+		<p className="link">Contact</p>
 		
 		</div>
 		<div>
@@ -19,23 +19,23 @@ function Footer(){
 		<img src="/src/images/support.png" />
 		<p className="title">Support</p>
 		</div>
-		<p>Contact Us</p>
-		<p>FAQS</p>
-		<p>Help Center</p>
+		<p className="link">Contact Us</p>
+		<p className="link">FAQS</p>
+		<p className="link">Help Center</p>
 		</div>
 		<div><div className="footer-title">	
 		<img src="/src/images/about.png" />
 		<p className="title">About</p>
 		</div>
-		<p>About Us</p>
-		<p>Privacy Policy</p>
+		<p className="link">About Us</p>
+		<p className="link">Privacy Policy</p>
 		</div>
 		<div><div className="footer-title">	
 		<img src="/src/images/social.png" />
 		<p className="title">Social Media</p>
 		</div>
-		<p>LinkedIn</p>
-		<p>YouTube</p>
+		<p className="link">LinkedIn</p>
+		<p className="link">YouTube</p>
 		</div>
 		</div>
 	)
