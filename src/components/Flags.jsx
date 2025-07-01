3f1ef14c79e26bf1b8566e88e0c326c2fd6626d9
paria <pariyataba@gmail.com>
2025-07-01 13:37:16 +0200

    add NavLink in first page

--- src/components/Flags.jsx
+++ src/components/Flags.jsx
@@ -1,12 +1,13 @@
+import { NavLink } from "react-router-dom"
 import "../components/Flags.css"
 function Flugs({flag}){
 	return(
 <div className="div-flags">
-	<div className="flag">
+	<NavLink className="flag">
 			<img src={flag.src} alt={flag.name} />
 	<p>{flag.name}</p>
 
-	</div>
+	</NavLink>
 </div>
 	)
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom"
 import "../components/Footer.css"
 function Footer(){
 	return(
@@ -7,35 +8,39 @@ function Footer(){
 		<img src="/src/images/ring.png" />
 		<p className="title">Quick Links</p>
 		</div>
-		<p className="link">Home</p>
-		<p className="link">Courses</p>
-		<p className="link">Blogs</p>
-		<p className="link">About us</p>
-		<p className="link">Contact</p>
-		
+		<div className="column">
+		<NavLink className="link">Home</NavLink>
+		<NavLink className="link">Courses</NavLink>
+		<NavLink className="link">Blogs</NavLink>
+		<NavLink className="link">About us</NavLink>
+		<NavLink className="link">Contact</NavLink>
+		</div>
 		</div>
 		<div>
 		<div className="footer-title">	
 		<img src="/src/images/support.png" />
 		<p className="title">Support</p>
 		</div>
-		<p className="link">Contact Us</p>
-		<p className="link">FAQS</p>
-		<p className="link">Help Center</p>
+		<div className="column">
+		<NavLink className="link">Contact Us</NavLink>
+		<NavLink className="link">FAQS</NavLink>
+		<NavLink className="link">Help Center</NavLink></div>
 		</div>
 		<div><div className="footer-title">	
 		<img src="/src/images/about.png" />
 		<p className="title">About</p>
 		</div>
-		<p className="link">About Us</p>
-		<p className="link">Privacy Policy</p>
+		<div className="column">
+		<NavLink className="link">About Us</NavLink>
+		<NavLink className="link">Privacy Policy</NavLink></div>
 		</div>
 		<div><div className="footer-title">	
 		<img src="/src/images/social.png" />
 		<p className="title">Social Media</p>
 		</div>
-		<p className="link">LinkedIn</p>
-		<p className="link">YouTube</p>
+		<div className="column">
+		<NavLink className="link">LinkedIn</NavLink>
+		<NavLink className="link">YouTube</NavLink></div>
 		</div>
 		</div>
 	)
